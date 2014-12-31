4e4403f131d259917f09fced403b5bbf57e5f93f
Jesse Zhu <jesse@itjesse.cn>
2014-12-31 22:05:31 +0800

    Init

---
+++ RFID Scanner/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RFID_Scaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbSerial = new System.Windows.Forms.ComboBox();
+            this.btnSerialOpen = new System.Windows.Forms.Button();
+            this.btnSerialClose = new System.Windows.Forms.Button();
+            this.resultList = new System.Windows.Forms.ListView();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.scanTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbSerial
+            // 
+            this.cbSerial.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSerial.FormattingEnabled = true;
+            this.cbSerial.Location = new System.Drawing.Point(12, 12);
+            this.cbSerial.Name = "cbSerial";
+            this.cbSerial.Size = new System.Drawing.Size(121, 20);
+            this.cbSerial.TabIndex = 0;
+            // 
+            // btnSerialOpen
+            // 
+            this.btnSerialOpen.Location = new System.Drawing.Point(139, 10);
+            this.btnSerialOpen.Name = "btnSerialOpen";
+            this.btnSerialOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnSerialOpen.TabIndex = 1;
+            this.btnSerialOpen.Text = "打开串口";
+            this.btnSerialOpen.UseVisualStyleBackColor = true;
+            this.btnSerialOpen.Click += new System.EventHandler(this.btnSerialOpen_Click);
+            // 
+            // btnSerialClose
+            // 
+            this.btnSerialClose.Enabled = false;
+            this.btnSerialClose.Location = new System.Drawing.Point(220, 10);
+            this.btnSerialClose.Name = "btnSerialClose";
+            this.btnSerialClose.Size = new System.Drawing.Size(75, 23);
+            this.btnSerialClose.TabIndex = 2;
+            this.btnSerialClose.Text = "关闭串口";
+            this.btnSerialClose.UseVisualStyleBackColor = true;
+            this.btnSerialClose.Click += new System.EventHandler(this.btnSerialClose_Click);
+            // 
+            // resultList
+            // 
+            this.resultList.FullRowSelect = true;
+            this.resultList.GridLines = true;
+            this.resultList.Location = new System.Drawing.Point(12, 38);
+            this.resultList.MultiSelect = false;
+            this.resultList.Name = "resultList";
+            this.resultList.Size = new System.Drawing.Size(760, 326);
+            this.resultList.TabIndex = 3;
+            this.resultList.UseCompatibleStateImageBehavior = false;
+            this.resultList.View = System.Windows.Forms.View.Details;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(12, 370);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(760, 44);
+            this.btnScan.TabIndex = 4;
+            this.btnScan.Text = "开始扫描";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // scanTimer
+            // 
+            this.scanTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 441);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.resultList);
+            this.Controls.Add(this.btnSerialClose);
+            this.Controls.Add(this.btnSerialOpen);
+            this.Controls.Add(this.cbSerial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbSerial;
+        private System.Windows.Forms.Button btnSerialOpen;
+        private System.Windows.Forms.Button btnSerialClose;
+        private System.Windows.Forms.ListView resultList;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Timer scanTimer;
+    }
+}
+
---
+++ RFID Scanner/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace RFID_Scaner
+{
+    public partial class Form1 : Form
+    {
+
+        private SerialPort SerialPort = new SerialPort();  
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //添加表头（列）
+            resultList.Columns.Add("序号", 40, HorizontalAlignment.Center);
+            resultList.Columns.Add("书名", 240, HorizontalAlignment.Center);
+            resultList.Columns.Add("作者", 240, HorizontalAlignment.Center);
+            resultList.Columns.Add("索书号", 240, HorizontalAlignment.Center);
+
+            //检查是否含有串口
+            string[] str = SerialPort.GetPortNames();
+            if (str.Length == 0)
+            {
+                MessageBox.Show("本机没有串口！", "Error");
+                Application.Exit();
+                return;
+            }
+
+            //添加串口项目
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                //获取有多少个COM口
+                cbSerial.Items.Add(s);
+            }
+
+            //串口设置默认选择项
+            cbSerial.SelectedIndex = 0;         //设置cbSerial的默认选项
+
+            //初始化SerialPort对象  
+            SerialPort.NewLine = "/r/n";
+            SerialPort.RtsEnable = false;
+            SerialPort.DataReceived += serialDataReceived; 
+        }
+
+        void serialDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int n = SerialPort.BytesToRead;//先记录下来，避免某种原因，人为的原因，操作几次之间时间长，缓存不一致  
+            byte[] buf = new byte[n];//声明一个临时数组存储当前来的串口数据   
+            SerialPort.Read(buf, 0, n);//读取缓冲数据  
+
+            //因为要访问ui资源，所以需要使用invoke方式同步ui。  
+            this.Invoke((EventHandler)(delegate
+            {
+                //直接按ASCII规则转换成字符串  
+                string result = Encoding.ASCII.GetString(buf);
+                resultList.BeginUpdate();
+
+                ListViewItem lvi = new ListViewItem();
+
+                lvi.Text = result;
+                resultList.Items.Add(lvi);
+
+                resultList.EndUpdate();
+                //MessageBox.Show(result, "result");
+            }));
+        }
+
+        private void btnSerialOpen_Click(object sender, EventArgs e)
+        {
+            //关闭时点击，则设置好端口，波特率后打开  
+            SerialPort.PortName = cbSerial.Text;
+            SerialPort.BaudRate = 115200;
+            SerialPort.DataBits = 8; //数据位
+            SerialPort.Parity = System.IO.Ports.Parity.None; //无奇偶校验位
+            SerialPort.StopBits = System.IO.Ports.StopBits.One; //一个停止位
+            SerialPort.ReadBufferSize = 1024; //接收缓冲区大小
+            try
+            {
+                SerialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                //捕获到异常信息，创建一个新的comm对象，之前的不能用了。  
+                SerialPort = new SerialPort();
+                //现实异常信息给客户。  
+                MessageBox.Show(ex.Message);
+            }
+            //设置按钮的状态  
+            btnSerialOpen.Enabled = false;
+            btnSerialClose.Enabled = true;
+            cbSerial.Enabled = false;
+            
+            Byte[] data = new Byte[3];
+            data[0] = 0x10;
+            data[1] = 0x03;
+            data[2] = 0x00;
+            SerialPort.Write(data, 0, 3);
+
+        }
+
+        private void btnSerialClose_Click(object sender, EventArgs e)
+        {
+            SerialPort.Close();
+            btnSerialOpen.Enabled = true;
+            btnSerialClose.Enabled = false;
+            cbSerial.Enabled = true;
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            if (scanTimer.Enabled)
+            {
+                scanTimer.Enabled = false;
+                btnScan.Text = "开始扫描";
+            }
+            else
+            {
+                scanTimer.Enabled = true;
+                btnScan.Text = "停止扫描";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Byte[] data = new Byte[3];
+            data[0] = 0x43;
+            data[1] = 0x03;
+            data[2] = 0x01;
+            SerialPort.Write(data, 0, 3);
+        }
+
+    }
+}
